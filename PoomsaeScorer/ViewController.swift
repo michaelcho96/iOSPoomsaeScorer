f1fdbc89c0d3deaa4a85367c060052c397f6f31c
Michael Cho <michaelcho96@berkeley.edu>
2015-12-23 01:11:20 -0800

    Initial Commit

---
+++ PoomsaeScorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoomsaeScorer
+//
+//  Created by Michael Cho on 12/23/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
